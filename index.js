74bf64a6fa8631dc7b6d01ff06fee82669713eef
abumalick <abdullah.hilson@gmail.com>
2019-05-31 16:31:40 +0000

    feat: use koa router and serve json witout extension

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const Koa = require('koa')
-const serve = require('koa-static')
+const send = require('koa-send')
+const Router = require('koa-router')
 const PORT = process.env.PORT || 5000
-
 const app = new Koa()
+const router = new Router()
 
-app.use(serve('./public'))
+// const oneDayMs = 1000 * 60 * 60 * 24
+// const oneYearMs = oneDayMs * 365
 
-app.use(async (ctx) => {
-  ctx.body = `
+router
+  .get('/', (ctx, next) => {
+    ctx.body = `
     <h1>بسم الله الرحمن الرحيم</h1>
     <h2>Ahlsunnah Radio API</h2>
-    <a href="./station-playlists.json">stations playlists</a>
+    <a href="./stations/">stations list</a>
   `
-})
+    next()
+  })
+  .get('/stations', (ctx) => {
+    return send(ctx, 'stations.json')
+  })
+  .get('/public/(.*)', (ctx) => {
+    return send(ctx, ctx.path, {
+      immutable: true,
+      // maxAge: oneYearMs,
+    })
+  })
+
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(PORT, () => {
   console.log('listening on http://localhost:' + PORT)
